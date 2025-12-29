88014e30dbb29868d03efcb1a1ee903455fce24a
Aniruddha Chaudhary <code.anirudh.dev@gmail.com>
2025-12-29 22:21:38 +0530

    feat: installed AWS-SECRETS CSI DRIVER using helm

---
+++ terraform/17-secrets-store-csi-driver.tf
@@ -0,0 +1,89 @@
+# This csi (container storage interface) driver can integrate secrets with many different cloud providers
+# secrets will be mounted as k8s volumes
+resource "helm_release" "secrets_csi_driver" {
+  name = "secrets-store-csi-driver"
+
+  repository = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts"
+  chart      = "secrets-store-csi-driver"
+  namespace  = "kube-system"
+  version    = "1.4.3"
+
+  # MUST be set if you use ENV variables
+  # this will create additional RBAC policies needed to use env variables
+  set = [{
+    name  = "syncSecret.enabled"
+    value = true
+  }]
+
+  depends_on = [helm_release.efs_csi_driver]
+}
+
+# cloud specific provider
+resource "helm_release" "secrets_csi_driver_aws_provider" {
+  name = "secrets-store-csi-driver-provider-aws"
+
+  repository = "https://aws.github.io/secrets-store-csi-driver-provider-aws"
+  chart      = "secrets-store-csi-driver-provider-aws"
+  namespace  = "kube-system"
+  version    = "0.3.9"
+
+  depends_on = [helm_release.secrets_csi_driver]
+}
+
+# openid trust policy for specific application
+data "aws_iam_policy_document" "myapp_secrets" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
+      #   myapp service account and 12-example is the namespace
+      values = ["system:serviceaccount:12-example:myapp"]
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.eks.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+# iam role for the specific application
+resource "aws_iam_role" "myapp_secrets" {
+  name               = "${aws_eks_cluster.eks.name}-myapp-secrets"
+  assume_role_policy = data.aws_iam_policy_document.myapp_secrets.json
+}
+
+# grant the application access to a specific secret in aws
+resource "aws_iam_policy" "myapp_secrets" {
+  name = "${aws_eks_cluster.eks.name}-myapp-secrets"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:DescribeSecret"
+        ]
+        # use secret arn to grant access to a specific secret
+        Resource = "*" # "arn:*:secretsmanager:*:*:secret:my-secret-kkargS"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "myapp_secrets" {
+  policy_arn = aws_iam_policy.myapp_secrets.arn
+  role       = aws_iam_role.myapp_secrets.name
+}
+
+output "myapp_secrets_role_arn" {
+  value = aws_iam_role.myapp_secrets.arn
+}
+
+# terraform apply
+# create a secret in aws secrets manager and obtain a secret name.
