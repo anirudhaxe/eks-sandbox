1cadc5c154573394b1baccaa09e8ceef6400e00d
Aniruddha Chaudhary <code.anirudh.dev@gmail.com>
2025-12-29 21:27:57 +0530

    feat: installed CERT MANAGER using helm

---
+++ terraform/13-cert-manager.tf
@@ -0,0 +1,19 @@
+// Install CERT MANAGER uisng helm
+resource "helm_release" "cert_manager" {
+  name = "cert-manager"
+
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  namespace        = "cert-manager"
+  create_namespace = true
+  version          = "v1.15.0"
+
+  set = [{
+    name  = "installCRDs"
+    value = "true"
+  }]
+
+  depends_on = [helm_release.external_nginx]
+}
+# terraform apply
+# kubectl get pods -n cert-manager
